66700ce640b1139d435c5bcb01c1406da147fb15
Elena <c.elena.palmer@gmail.com>
2023-08-05 10:59:22 -0500

    Restructure package

--- setup.py
+++ setup.py
@@ -1,48 +1,44 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-import codecs
-import os
-
-from setuptools import find_packages, setup
-
-
-def local_file(name):
-    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
-
-
-SOURCE = local_file('src')
-README = local_file('README.rst')
-long_description = codecs.open(README, encoding='utf-8').read()
-
+from setuptools import setup
 
 setup(
-    name='ao3',
-    version='0.2.0',
-    description='A Python API for scraping AO3 (the Archive of Our Own)',
-    long_description=long_description,
-    url='https://github.com/alexwlchan/ao3',
-    author='Alex Chan',
-    author_email='alex@alexwlchan.net',
-    license='MIT',
+    name='ourchive-ao3-importer',
+    version='0.1.0',
+    description='An AO3 downloader, used in Ourchive code, forked from ladyofthelog and alexwlchan.',
+    url='https://github.com/c-e-p/ao3importer',
+    author='Elena',
+    author_email='imperfectelena@protonmail.com',
+    license='MIT license',
+    packages=['ourchive-ao3-importer'],
+    install_requires=['beautifulsoup4>=4.12.2',
+                      'certifi>=2023.5.7',
+                      'cffi>=1.15.1',
+                      'chardet>=5.1.0',
+                      'charset-normalizer>=3.1.0',
+                      'colorama>=0.4.6',
+                      'cryptography>=41.0.1',
+                      'cssselect>=1.2.0',
+                      'EbookLib>=0.18',
+                      'idna>=3.4',
+                      'loguru>=0.6.0',
+                      'lxml>=4.9.2',
+                      'mobi>=0.3.3',
+                      'pdfminer.six>=20221105',
+                      'pdfquery>=0.4.3',
+                      'pycparser>=2.21',
+                      'pyquery>=2.0.0',
+                      'requests>=2.31.0',
+                      'roman>=4.1',
+                      'six>=1.16.0',
+                      'soupsieve>=2.4.1',
+                      'tqdm>=4.65.0',
+                      'urllib3>=2.0.3',
+                      'win32-setctime>=1.1.0'],
 
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    packages=find_packages(SOURCE),
-    package_dir={'': SOURCE},
-    install_requires=[
-        'beautifulsoup4>=4.5.3, <5',
-        'requests>=2.12.4, <3',
+        'Development Status :: 2 - MVP',
+        'Intended Audience :: Fans',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.11',
     ],
 )

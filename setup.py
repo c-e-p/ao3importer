0ba0230bc1dfaca38fd42e1cdda22fe67cae36b1
Elena <c.elena.palmer@gmail.com>
2023-08-05 12:51:37 -0500

    Change module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='ourchive-ao3-importer',
+    name='ourchiveao3importer',
     version='0.1.0',
     description='An AO3 downloader, used in Ourchive code, forked from ladyofthelog and alexwlchan.',
     url='https://github.com/c-e-p/ao3importer',

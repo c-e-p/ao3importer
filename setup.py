5bd0694b4f8007efcc3e4b840f7d57eefedd2f13
Elena <c.elena.palmer@gmail.com>
2023-08-24 17:51:54 -0500

    Update setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ourchiveao3importer',
-    version='0.1.0',
+    version='0.1.2',
     description='An AO3 downloader, used in Ourchive code, forked from ladyofthelog and alexwlchan.',
     url='https://github.com/c-e-p/ao3importer',
     author='Elena',
